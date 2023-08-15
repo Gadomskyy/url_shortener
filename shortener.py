57d269791cadc79a5d927c224f3419e9306e492d
Gadomskyy <j_gadomski@hotmail.com>
2023-08-15 22:23:38 +0200

    Add generate_short_url function

--- shortener.py
+++ shortener.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import string
+import random
 
 
 #https://flask.palletsprojects.com/en/2.3.x/
@@ -7,3 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def shortener():
     return "<p>URL Shortener</p>"
+
+
+def generate_short_url(length = 6):
+    chars = string.ascii_letters + string.digits
+    short_url = "".join([random.choice(chars) for x in range(length)])
+    return short_url
