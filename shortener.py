40aa03fbe45aa68d0407d2150dd96addf859587c
Gadomskyy <j_gadomski@hotmail.com>
2023-08-17 22:32:37 +0200

    create DB instance using SQLAlchemy

--- shortener.py
+++ shortener.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import string
 import random
 
-
 #https://flask.palletsprojects.com/en/2.3.x/
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class Links(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(500), nullable=False)
+    generated_url_end = db.Column(db.String(6), nullable=False)
+    short_url = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"Long link: {self.long_url}, short link: {self.short_url}"
+
 
 @app.route("/", methods= ['GET', 'POST'])
 def mainpage():
@@ -12,7 +25,7 @@ def mainpage():
         long_url = request.form['long_url']
         short_url = f"{request.url_root}{generate_short_url()}"
         return f"""
-        Long URL: {long_url}
+        Long URL: {long_url}\n
         Shortened URL: {short_url}"""
     return render_template('mainpage.html')
 
